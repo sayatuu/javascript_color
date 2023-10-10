089409c0ddd44d57407a62574a1995c547e195cd
sayatuu <107912580+sayatuu@users.noreply.github.com>
2023-10-10 16:35:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // JavaScript
-console.log(document.getElementById('myColor').value);
-
-document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
+//console.log(document.getElementById('myColer').value)
 
 //定数の宣言
 const color = document.getElementById('myColor');
 const text = document.getElementById('bodyText');
 
-document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
 
-text.textContent = color.value;
+function changeColor(){
+  //背景色を表示
+  document.body.style.backgroundColor = color.value;
+  //カラーコードを表示
+  if (color.value === '#ffffff') {
+    text.textContent = 'コード：' + color.value + '(white)';
+  } else if (color.value === '#ff0000') {
+    text.textContent = 'コード：' + color.value + '(red)';
+  } else {
+    text.textContent = 'コード：' + color.value;
+  }
+}
+
+// カラーピッカーが変更されたときに、背景色を変える
+color.addEventListener('input', changeColor);
